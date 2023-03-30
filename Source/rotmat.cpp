434f478df49d84567ff6db03ad1c66727dce5d80
Mathias Balling <math260r@gmail.com>
2023-03-30 17:07:01 +0200

    30-03-2023 - 17:06

--- Source/rotmat.cpp
+++ Source/rotmat.cpp
@@ -1,14 +1,17 @@
-#include "../Header/rotmat.h"
+#include "rotmat.h"
+#include <cmath>
 
-RotMat2D::RotMat2D() : Matrix(2, 2, 0), angle(0)
+RotMat2D::RotMat2D() : Matrix(2, 2, 0)
 {
     at(0, 0) = 1;
     at(1, 1) = 1;
 }
 RotMat2D::RotMat2D(double angle) : Matrix(2, 2, 0), angle(angle)
 {
-    at(0, 0) = 1;
-    at(1, 1) = 1;
+    at(0, 0) = cos(angle);
+    at(0, 1) = -sin(angle);
+    at(1, 0) = sin(angle);
+    at(1, 1) = cos(angle);
 }
 
 void RotMat2D::print()
